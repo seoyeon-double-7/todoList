ff83f7a041e550741348a118d09797a2e3120fb8
seoyeon-double-7 <s2103@e-mirim.hs.kr>
2023-01-22 18:55:26 +0900

    quotes and backgound img

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const images = [
+    "0.jpeg",
+    "1.jpeg",
+    "2.jpeg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+bgImage.className = `bgimage`;
+document.body.appendChild(bgImage);
